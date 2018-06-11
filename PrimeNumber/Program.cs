d02318470fa8ca505655d85dc979576de757805d
Łukasz Tomalczyk <lukasz.tomalczyk@outlook.com>
2018-06-11 16:14:22 +0200

    start work on stack array

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isPrime;
+            List<int> primeNumbers = new List<int>(new int[] { 2 });
+            for (int i = 3; i < 100; i++)
+            {
+                isPrime = true;
+                for (int j = 0; j < primeNumbers.Count; j++)
+                    if ((i % primeNumbers[j]) == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                if (isPrime) primeNumbers.Add(i);
+            }
+            foreach (int i in primeNumbers)
+                Console.Write(i + ", ");
+            Console.WriteLine();
+        }
+    }
+}
